00f602f49c761dfdc913f998c462d90f7fbdefe5
CristianTelega <telegacristian@gmail.com>
2024-10-22 22:04:22 -0300

    Sign In y JsonServer
    Se finalizo User Story (Scrum-81), se creo un Json Server para guardar los usuario, se avanza con (SCRUM-56, SCRUM-80 Y SCRUM-83)

--- src/app/Services/user.service.ts
+++ src/app/Services/user.service.ts
@@ -1,25 +1,33 @@
 
 import { Injectable } from '@angular/core';
 import { User } from '../Interface/user';
+import { HttpClient } from '@angular/common/http';
+import { lastValueFrom, Observable } from 'rxjs';
 
 
 @Injectable({
   providedIn: 'root'
 })
 export class UserService {
+
+  private apiUsuariosUrl = 'http://localhost:3000/users';
   
-public userList: User[] = [];
 
-constructor(){}
 
-registerUser(user: User): boolean {
-  user.id = this.userList.length + 1;
-  this.userList.push(user);
-  console.log(this.userList);
-  return true;
+constructor(private http: HttpClient){}
+
+registerUser(user: User): Promise<User>{
+
+ return lastValueFrom(this.http.post<User>(this.apiUsuariosUrl, user));
 }
 
 
+
+comprobarUserNameExiste(username: string): Observable<User[]> {
+
+  return this.http.get<User[]>(`${this.apiUsuariosUrl}?username=${username}`);
+  
+}
 
 
 
