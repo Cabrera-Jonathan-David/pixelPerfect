ac4ca43b7ca5e8c64a58e669b3d36f0da3b6910e
Your Name <cabrera.jonathan.d@gmail.com>
2024-11-07 18:21:34 -0300

    CONFIRMAR CAMBIOS

---
+++ src/app/Interface/payment-register.ts
@@ -0,0 +1,12 @@
+export interface PaymentProduct {
+    productId: string;
+    price: number;
+    quantity: number;
+  }
+  
+  export interface PaymentRegister {
+    id: number;
+    products: PaymentProduct[];
+    userId: string | null;
+    estado: string; 
+  }
